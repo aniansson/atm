b9a76b56b200a8482d76f41480422b24c246247a
blondabambi <ebba@aniansson.se>
2016-11-16 16:49:47 +0100

    done with the account

---
+++ lib/account.rb
@@ -0,0 +1,36 @@
+class Account
+  attr_accessor :exp_date, :pin_code, :balance, :account_status, :owner
+
+def initialize(attrs = {})
+  @exp_date = set_expire_date
+  @pin_code = generate_pin
+  @balance = 0
+  @account_status = :active
+  set_owner(attrs[:owner])
+end
+
+  STANDARD_VALIDITY_YRS = 5
+
+def set_expire_date
+  Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+end
+
+def self.deactivate(account)
+  account.account_status = :deactivate
+end
+
+
+  private
+    def generate_pin
+      pin_code = 1234
+    end
+
+    def set_owner(obj)
+      obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner
+      raise "An Account owner is required"
+    end
+
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,39 @@
+require './lib/account.rb'
+
+describe Account do
+  let(:person) {instance_double('Person', name: 'Ebba')}
+  subject { described_class.new({owner: person}) }
+
+  it 'pin needs to have four digits' do
+    number = 1234
+    number_length = Math.log10(number).to_i + 1
+    expect(number_length).to eq 4
+  end
+
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime('%m/%Y')
+    expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is required to have a balance' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+  it 'deactivates account using Class method' do
+    Account.deactivate(subject)
+    expect(subject.account_status).to eq :deactivate
+  end
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
+  end
+
+end
