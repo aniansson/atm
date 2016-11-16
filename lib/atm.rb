8d6690666f18c8d791c540c021394d20cc464c2f
blondabambi <ebba@aniansson.se>
2016-11-16 12:29:52 +0100

    done with sad path, yeey

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,12 +1,11 @@
 class ATM
  attr_accessor :funds
- attr_accessor :balance
 
  def initialize
    @funds = 1000
  end
 
- def withdraw(amount, pin_code, account)
+ def withdraw(amount, pin_code, account, account_status)
    case
    when insufficient_funds_in_account?(amount, account)
      { status: false, message: 'insufficient funds in account', date: Date.today }
@@ -14,6 +13,10 @@ class ATM
   { status: false, message: 'insufficient funds in ATM', date: Date.today }
 when incorrect_pin?(pin_code, account.pin_code)
   { status: false, message: 'wrong pin', date: Date.today }
+when card_expired?(account.exp_date)
+  { status: false, message: 'card expired', date: Date.today}
+when account_disabled?(account_status)
+  { status: false, message: 'account disabled', date: Date.today }
    else
      perform_transaction(amount, account)
    end
@@ -38,4 +41,12 @@ end
    account.balance = account.balance - amount
    { status: true, message: 'success', date: Date.today, amount: amount }
  end
+
+ def card_expired?(exp_date)
+   Date.strptime(exp_date, '%m/%y') < Date.today
+ end
+
+ def account_disabled?(account_status)
+  account_status != :active
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe ATM do
-  let(:account) { instance_double('Account', pin_code: '1234') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/17', account_status: :active) }
 
   before do
     allow(account).to receive(:balance).and_return(100)
@@ -13,23 +13,39 @@ describe ATM do
   end
 
   it 'funds are reduced at withdraw' do
-    subject.withdraw(50, '1234', account)
+    subject.withdraw(50, '1234', account, account.account_status)
     expect(subject.funds).to eq 950
   end
 
   it 'allow withdraw if account has enough balance.' do
-    expect_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-    expect(subject.withdraw(45, account)).to eq expect_output
+    expect_output = {
+      status: true,
+      message: 'success',
+      date: Date.today,
+      amount: 45 }
+    expect(subject.withdraw(45, '1234', account, account.account_status)).to eq expect_output
   end
 
   it 'reject withdraw if account has insufficient funds' do
     subject.funds = 50
     expect_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    expect(subject.withdraw(100, account)).to eq expect_output
+    expect(subject.withdraw(100, '1234', account, account.account_status)).to eq expect_output
   end
 
   it 'reject withdraw if pin is wrong' do
     expected_output = { status: false, message: 'wrong pin', date: Date.today }
-    expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    expect(subject.withdraw(50, 9999, account, account.account_status)).to eq expected_output
+  end
+
+  it 'reject withdraw if card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(10, '1234', account, account.account_status)).to eq expected_output
+  end
+
+  it 'reject withdraw if account is disabled' do
+    allow(account).to receive(:account_status).and_return('disabled')
+    expected_output = { status: false, message: 'account disabled', date: Date.today }
+    expect(subject.withdraw(10, '1234', account, :disabled)).to eq expected_output
   end
 end
