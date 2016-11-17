a1e42bc17c57314c07eed40659b5481ea1cbe609
blondabambi <ebba@aniansson.se>
2016-11-17 17:31:53 +0100

    im fucking done, kinda

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,12 @@
+require './lib/account'
+
 class Person
   attr_accessor :name, :cash, :account
 
 def initialize (attrs = {})
-  set_name(attrs[:name])
-  @cash = 0
+  @name = set_name(attrs[:name])
+  @cash = 100
+  # @account = nil
 end
 
 def account_attribute
@@ -18,8 +21,13 @@ def deposit(amount)
   if @account.nil?
     raise 'No account present'
   else
-    @cash += amount
+    @cash -= amount
+    @account.balance += amount
   end
+end
+
+def withdraw(args = {})
+  @account == nil ? missing_account : withdraw_funds(args)
 end
 
 private
@@ -30,6 +38,32 @@ end
 
 def missing_name
   raise "A name is required"
+end
+
+def deposit_funds(amount)
+  @cash -= amount
+  @account.balance += amount
+end
+
+def withdraw_funds(args)
+  args[:atm] == nil ? missing_atm : atm = args[:atm]
+  account = @account
+  amount = args[:amount]
+  pin = args[:pin]
+  response = atm.withdraw(amount, pin, account, account.account_status)
+  response[:status] == true ? increase_cash(response) : response
+end
+
+def increase_cash(response)
+  @cash += response[:amount]
+end
+
+def missing_account
+  raise RuntimeError, 'No account present'
+end
+
+def missing_atm
+  raise RuntimeError, 'An ATM is required'
 end
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,7 +14,7 @@ describe Person do
   end
 
   it 'is expected to have a :cash attribute with value of 0 on initialize' do
-    expect(subject.cash).to eq 0
+    expect(subject.cash).to eq 100
   end
 
   it 'is expected to have an :account attribute' do
@@ -40,11 +40,36 @@ describe Person do
      it 'can deposit funds' do
        expect(subject.deposit(100)).to be_truthy
      end
+
+     it 'funds are added to the account balance - deducted from cash' do
+       subject.cash = 100
+       subject.deposit(100)
+       expect(subject.account.balance).to be 100
+       expect(subject.cash).to be 0
+     end
+
+     it 'can withdraw funds' do
+       command = lambda { subject.withdraw(amount: 100, pin: '1234', account: subject.account, atm: atm) }
+       expect(command.call).to be_truthy
+     end
+
+     it 'withdraw is expected to raise error if no ATM is passed in' do
+       command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account) }
+       expect { command.call }.to raise_error 'An ATM is required'
+     end
+
+     it 'funds are added to cash - deducted from account balance' do
+       subject.cash = 100
+       subject.withdraw(amount: 100, pin: '1234', account: subject.account, atm: atm)
+       expect(subject.account.balance).to be 0
+       expect(subject.cash).to be 100
+     end
    end
 
   describe 'can not manage funds if no account has been created' do
      it 'can\'t deposit funds' do
        expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
      end
+
   end
 end
